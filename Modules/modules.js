5294216afdf6068e958d5b2e8f2dd629c804542d
Amrendra Singh <happysingh0987688@gmail.com>
2025-11-26 09:38:41 +0530

    Modules

---
+++ Modules/modules.js
@@ -0,0 +1,7 @@
+const math = require('./math'); //  Importing the math module
+
+const sum = math.add(5, 3);
+const difference = math.sub(10, 4);
+
+console.log("Sum:",sum);
+console.log("Difference:",difference);
